d798a423cefdadf50f3003f44b7e7a35cfeac025
isergeich22 <skiing1r@gmail.com>
2025-10-14 22:27:18 +0300

    update actual_marks; short_report

--- app.js
+++ app.js
@@ -1758,7 +1758,7 @@ app.get('/ozon/:from', async function(req, res){
 
     html += `</section>
              <section class="action-form">
-                <button id="current-order"><a href="http://localhost:3030/ozon_marks_order" target="_blank">Создать заказы маркировки</a></button>
+                <button id="current-order"><a href="http://localhost:3030/ozon_marks_order/${req.params.from}" target="_blank">Создать заказы маркировки</a></button>
              </section>
              <div class="body-wrapper"></div>                        
              ${footerComponent}`
