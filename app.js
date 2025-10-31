9c271ee0a96b12c1a79f372b2edbba7f3e0cc067
isergeich22 <skiing1r@gmail.com>
2025-10-31 19:33:18 +0300

    update actual_marks; short_report

--- app.js
+++ app.js
@@ -2201,6 +2201,8 @@ app.get('/wildberries', async function(req, res){
 
     for(let i = 0; i < wb_orders.length; i++) {
 
+        console.log(wb_orders[i].vendor)
+
         const response = await axios.post('https://api-seller.ozon.ru/v4/product/info/attributes', {
                     
             "filter": {
